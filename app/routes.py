9170adad05ca8efa54b401a041294a116a96faaf
Bartek <kowalczuk.bartek@gmail.com>
2019-01-12 12:54:09 +0100

    Add forms

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,45 @@
-from flask import render_template, Flask, Markup
 from app import app
+from flask import render_template, Flask, Markup, flash, redirect, request
+from flask_table import Table, Col
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import io
 import base64
-from flask_table import Table, Col
 
 from scraper import cryptoInfoToDf
 from sqlCrypto import executeSqlCrypto
+from forms import LoginForm
 
-colors = [
-    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
-    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
-    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
+@app.route('/')
+@app.route('/form')
+def print_form():
+    return render_template('form.html')
+
+
+@app.route("/cryptoSummary/")
+@app.route("/cryptoSummary")
+def summary():
+
+    currency = request.args.get('currency')
+    fromDate = request.args.get('fromDate')
+    tillDate = request.args.get('tillDate')
+
+    df = executeSqlCrypto()
+
+    img = io.BytesIO()
+    sns.set(style="darkgrid")
+    fig, ax =  plt.subplots(1,2, figsize=(11,6))
+    sns.regplot(x=df["Volume"], y=df["Market Cap"], ax=ax[0])
+    sns.boxplot(x=df["Volume"], y= None, ax=ax[1])
+    fig.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    return render_template('cryptoSummary.html', currency=currency, fromDate=fromDate, tillDate=tillDate, df=df, plot_url=plot_url)
+
 
 
 @app.route('/cryptoCharts/')
@@ -39,20 +64,3 @@ def tables():
     df = executeSqlCrypto()
     return render_template('cryptoTables.html',
                            tables=df.to_html(classes=["table table-bordered table-hover"]))
-
-
-@app.route("/cryptoSummary/")
-@app.route("/cryptoSummary")
-def summary():
-    df = executeSqlCrypto()
-
-    img = io.BytesIO()
-    sns.set(style="darkgrid")
-    fig, ax =  plt.subplots(1,2, figsize=(11,6))
-    sns.regplot(x=df["Volume"], y=df["Market Cap"], ax=ax[0])
-    sns.boxplot(x=df["Volume"], y= None, ax=ax[1])
-    fig.savefig(img, format='png')
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()
-
-    return render_template('cryptoSummary.html', df=df, plot_url=plot_url)
